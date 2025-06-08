5c2d8e33d386b6af8d2be794eadeb0825304dbdb
Soumadeep Shee <soumadeepshee7@gmail.com>
2025-06-08 10:45:00 +0530

    intilize

---
+++ odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number");
+    scanf("%d",&n);
+    if(n%2!=0){
+        printf("The Number is odd");
+
+    }
+    else{
+        printf("The number is even");
+    }
+}
