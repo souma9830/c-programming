25b8f8dddeed6444146ad05c378ffee1ac1c579c
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-06-08 10:23:07 +0530

    Modified add.c

--- add.c
+++ add.c
@@ -6,7 +6,7 @@ int main()
     int res[2][2];
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
-            res[i][j]=arr1[i][j]+arr2[i][j];
+            res[i][j]=arr1[i][j]-arr2[i][j];
         }
         printf("\n");
     }
