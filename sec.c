aebbffebfae5a304d4c635b1d0315659bf103295
Soumadeep Shee <soumadeepshee7@gmail.com>
2025-06-09 13:00:52 +0530

    initial

--- sec.c
+++ sec.c
@@ -11,7 +11,7 @@ int main(){
     for(int i=0;i<n-1;i++){
         int min=INT_MAX;
         int minidx=-1;
-        for(int j=0;j<=n-1;j++){
+        for(int j=i;j<=n-1;j++){
             if(min>arr[j]){
                 min=arr[j];
                 minidx=j;
